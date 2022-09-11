610b27e8ad3fbe81ab56704392934b1ee0bd2199
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-09-11 15:05:43 +0700

    Loop for updating something

--- simengine/tools.py
+++ simengine/tools.py
@@ -1,5 +1,17 @@
+from time import sleep
+from typing import Iterable
+
+from interfaces import IUpdatable
 
 
+class LoopUpdater:
+    def __init__(self, updated_object: IUpdatable, timeout: int = 0):
+        self.updated_object = updated_object
+        self.timeout = timeout
 
+    def run(self) -> None:
+        while True:
+            self.updated_object.update()
 
-
+            if self.timeout > 0:
+                sleep(self.timeout)
