d7b09d3203e37f24a9a8eca39c8fd7642fc5cc45
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-09-11 15:03:08 +0700

    Removing unnecessary code

--- simengine/tools.py
+++ simengine/tools.py
@@ -1,23 +1,5 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
-from typing import Callable
 
 
-@dataclass(frozen=True)
-class SpawnContainer:
-    """Postpones object creation."""
-
-    factory: Callable
-    args_for_factory: tuple = tuple()
-    kwargs_for_factory: dict = field(default_factory=dict)
-    meta_information: dict = field(default_factory=dict)
-
-    def __call__(self) -> any:
-        return self.factory(*self.args_for_factory, **self.kwargs_for_factory)
 
 
-@dataclass(frozen=True)
-class RGBColor:
-    red: int = 0
-    green: int = 0
-    blue: int = 0
+
