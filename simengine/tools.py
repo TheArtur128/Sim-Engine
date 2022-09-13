4d1248a2533721115f1e3852ececf78d2bdfed66
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-09-13 20:43:31 +0700

    Rounding functions

--- simengine/tools.py
+++ simengine/tools.py
@@ -1,5 +1,6 @@
 from time import sleep
 from typing import Iterable
+from math import ceil
 
 from interfaces import IUpdatable
 
@@ -15,3 +16,36 @@ class LoopUpdater:
 
             if self.timeout > 0:
                 sleep(self.timeout)
+
+
+def round_number_with_comma_shift(number: int | float, comma_shift: int) -> float:
+    return _move_point_in_number(
+        round(_move_point_in_number(number, comma_shift)),
+        -comma_shift
+    )
+
+
+def round(number: int | float) -> float:
+    number_after_point = int(str(float(number)).split('.')[1][0])
+
+    if number_after_point >= 5:
+        return int(number) + 1
+    else:
+        return int(number)
+
+
+def _move_point_in_number(number: int | float, shift: int) -> float: # Will be redone
+    letters_of_number = list(str(float(number)))
+    point_index = letters_of_number.index('.')
+    letters_of_number.pop(point_index)
+
+    point_index += shift
+
+    if point_index > len(letters_of_number):
+        point_index = len(letters_of_number)
+    elif point_index < 0:
+        point_index = 0
+
+    letters_of_number.insert(point_index, '.')
+
+    return float(''.join(letters_of_number))
