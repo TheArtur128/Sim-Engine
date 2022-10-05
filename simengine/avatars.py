da24a778f02034ec85b2453fe42e744676174931
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-10-05 21:48:41 +0700

    Animation class

--- simengine/avatars.py
+++ simengine/avatars.py
@@ -8,6 +8,7 @@ from simengine.core import PositionalUnit
 from simengine.geometry import Vector
 from simengine.renders import ResourcePack
 from simengine.interfaces import IAvatar
+from simengine.errors.avatar_errors import AnimationAlreadyFinishedError
 
 
 class Avatar(IAvatar, ABC):
@@ -68,3 +69,44 @@ class Sprite:
 
     def __post_init__(self):
         self.real_stay_ticks = self.max_stay_ticks
+
+
+class Animation(SingleResourcePackAvatar, ABC):
+    _sprites: tuple[Sprite, ]
+    _current_sprite_index: int = 0
+
+    def __init__(self, unit: PositionalUnit):
+        super().__init__(unit)
+        self._update_main_resource_pack()
+
+    def update(self) -> None:
+        super().update()
+
+        if self.is_finished():
+            self._handle_finish()
+
+        self._update_main_resource_pack()
+
+        self._active_sprite.real_stay_ticks -= 1
+
+        if self._active_sprite.real_stay_ticks <= 0:
+            self._current_sprite_index += 1
+
+    def is_finished(self) -> bool:
+        return (
+            self._current_sprite_index >= len(self._sprites) - 1
+            and self._active_sprite.real_stay_ticks <= 0
+        )
+
+    @property
+    def _active_sprite(self) -> Sprite:
+        return self._sprites[self._current_sprite_index]
+
+    def _update_main_resource_pack(self) -> None:
+        self._main_resource_pack = ResourcePack(
+            self._sprites[self.__current_sprite_index].resource,
+            self.unit.position
+        )
+
+    def _handle_finish(self) -> None:
+        raise AnimationAlreadyFinishedError(f"Animation {self} already finished")
