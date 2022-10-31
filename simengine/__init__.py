b46b6d11d307d6715eb2f70e354b9e5439b2b875
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-10-31 19:11:52 +0700

    Minor script optimization

--- simengine/__init__.py
+++ simengine/__init__.py
@@ -4,16 +4,16 @@ from os import *
 current_path = path.dirname(path.abspath(__file__))
 current_directory = path.basename(current_path)
 
-file_names_of_module = [
+file_names_of_module = (
     file_name for file_name in listdir(current_path)
     if '.py' == file_name[-3:]
-]
+)
 
 ignore_fiele_names = ('__init__.py', '__pycache__')
 
-for ignore_fiele_name in ignore_fiele_names:
-    if ignore_fiele_name in file_names_of_module:
-        file_names_of_module.remove(ignore_fiele_name)
-
-for file_name in file_names_of_module:
+for file_name in (
+    file_name
+    for file_name in file_names_of_module
+    if file_name not in ignore_fiele_names
+):
     exec(f"from {current_directory}.{file_name.split('.')[0]} import *")
