37730a215566ea98cce2ceb49162c2671642f7ea
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-05 15:16:57 +0700

    Documentation for pygame render resources

--- pygame_renders/resources.py
+++ pygame_renders/resources.py
@@ -7,17 +7,28 @@ from simengine.tools import RGBAColor
 
 @dataclass
 class GraphicPrimitive:
+    """Dataclass of pygame render resources with color."""
+
     color: RGBAColor
 
 
 @dataclass
 class Polygon(GraphicPrimitive):
+    """Dataclass containing data for pygame polygon drawing function."""
+
     points: Iterable[Vector, ]
     border_width: int | float = 0
 
 
 @dataclass
 class Line(GraphicPrimitive):
+    """
+    Dataclass containing data for pygame line and aaline drawing functions.
+
+    The value of is_smooth attribute defines the annotation on the using function:
+    True - aaline, False - line.
+    """
+
     start_point: Vector
     end_point: Vector
     border_width: int | float = 1
@@ -26,6 +37,13 @@ class Line(GraphicPrimitive):
 
 @dataclass
 class Lines(GraphicPrimitive):
+    """
+    Dataclass containing data for pygame lines and aalines drawing functions.
+
+    The value of is_smooth attribute defines the annotation on the using function:
+    True - aalines, False - lines.
+    """
+
     is_closed: bool
     points: Iterable[Vector, ]
     border_width: int | float = 1
@@ -34,28 +52,38 @@ class Lines(GraphicPrimitive):
 
 @dataclass
 class Circle(GraphicPrimitive):
+    """Dataclass containing data for pygame circle drawing function."""
+
     radius: int | float
     border_width: int | float = 0
 
 
 @dataclass
 class CornerZone(GraphicPrimitive):
+    """GraphicPrimitive dataclass containing the size of the area of the drawn figure."""
+
     width: int | float
     height: int | float
 
 
 @dataclass
 class Rectangle(CornerZone):
+    """Dataclass containing data for pygame rect drawing function."""
+
     border_width: int | float = 0
 
 
 @dataclass
 class Ellipse(CornerZone):
+    """Dataclass containing data for pygame ellipse drawing function."""
+
     border_width: int | float = 0
 
 
 @dataclass
 class Arc(CornerZone):
+    """Dataclass containing data for pygame arc drawing function."""
+
     start_angle: int | float
     stop_angle: int | float
     border_width: int | float = 1
